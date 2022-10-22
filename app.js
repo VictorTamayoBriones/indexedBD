4f2b11e7ff3e2aa77e3c894090a58a2a2b664f59
Victor Manuel Tamayo Briones <victortamayo509@gmail.com>
2022-10-21 22:11:26 -0500

    handle erros

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ request.onupgradeneeded = e => {
         keyPath: 'id'
     })
 }
+
+//manejo de errores
+request.onerror = e => {
+    console.log('DB error: ', e.target.error);
+} 
+
+//insertar datos 
+request.onsuccess = e => {
+    let db = e.target.result;
+    let heroesData = [
+        {id: '111', heroe: 'Spiderman', mensaje:'Aqui Spiderman'},
+        {id: '222', heroe: 'Hulk', mensaje:'Aqui Hulk'},
+        {id: '222', heroe: 'Ironman', mensaje:'Aqui Ironman'}
+    ];
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+    heroesTransaction.onerror = e => {
+        console.log('Error guardado', e.target.error);
+    };
+    //informa sobre el exito de la transaccion
+    heroesTransaction.oncomplete = e => {
+        console.log('Transaccion Hecha', e);
+    };
+    let heroesStore = heroesTransaction.objectStore('heroes');
+    for(let heroe of heroesData){
+        heroesStore.add(heroe);
+    };
+    heroesStore.onsuccess = e => {
+        console.log('Nuevo item agregado');
+    };
+}
